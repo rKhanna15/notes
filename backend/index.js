341e753ad0b42fa5e8e3ce748e134aeded262639
rKhanna15 <riteshkhanna1504@gmail.com>
2023-09-06 09:17:56 -0700

    Third Commit

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+const static = require('static')
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 
 let notes = [
